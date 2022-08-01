e4161959e497f9fa70ab84eccfbafbf70e20c5a5
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-31 20:06:11 -0500

    added more comprehensible info page

--- routes/api/minibrand.js
+++ routes/api/minibrand.js
@@ -6,57 +6,63 @@ const _ = require('lodash')
 
 const minibrandTotal = minibrand.length
 
-//Get all Mini Brands Series 3
+//Get all Mini Brands All Series
 router.get('/', (req, res) => {
-    res.json(minibrand)})
+    res.json(minibrand)
+})
 
 //Get mini brand by series number
 router.get('/seriesno/:seriesNo', (req, res) => {
     const minibrandSeriesNo = req.params.seriesNo;
-    const found = minibrand.some(minibrand => minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') === minibrandItemNo.toString().toLowerCase().replace(/\s/g, ''));
+    const found = minibrand.some(minibrand => minibrand.seriesNo.toString().toLowerCase().replace(/\s/g, '') === minibrandSeriesNo.toString().toLowerCase().replace(/\s/g, ''));
     
     if(found) {
-        res.json(minibrand.filter(minibrand => minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') === minibrandItemNo.toString().toLowerCase().replace(/\s/g, '')));
+        res.json(minibrand.filter(minibrand => minibrand.seriesNo.toString().toLowerCase().replace(/\s/g, '') === minibrandSeriesNo.toString().toLowerCase().replace(/\s/g, '')));
     } else {
         res.status(400).json(
             {
-                ErrorMsg: `${_.startCase(_.toLower(minibrandItemNo))} is not a itemNo in our database.` 
+                ErrorMsg: `${_.startCase(_.toLower(minibrandSeriesNo))} is not a seriesNo in our database.` 
             }
         );
     }
 })
 
-//Get mini brands by item number
-router.get('/itemno/:itemNo', (req, res) => {
-    const minibrandItemNo = req.params.itemNo;
-    const found = minibrand.some(minibrand => minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') === minibrandItemNo.toString().toLowerCase().replace(/\s/g, ''));
-    
-    if(found) {
-        res.json(minibrand.filter(minibrand => minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') === minibrandItemNo.toString().toLowerCase().replace(/\s/g, '')));
-    } else {
-        res.status(400).json(
-            {
-                ErrorMsg: `${_.startCase(_.toLower(minibrandItemNo))} is not a itemNo in our database.` 
-            }
-        );
-    }
-});
+// // //Get mini brands by item number
+// // router.get('/:seriesNo/itemno/:itemNo', (req, res) => {
+// //     const minibrandItemNo = req.params.itemNo;
+// //     const minibrandSeriesNo = req.params.seriesNo;
 
-//find mini brands by rarity
-router.get('/rarity/:rarity', (req, res) => {
-    const minibrandRarity = req.params.rarity;
-    const found = minibrand.some(minibrand => minibrand.rarity.toString().toLowerCase().replace(/\s/g, '') === minibrandRarity.toString().toLowerCase().replace(/\s/g, ''));
+// //     const found = minibrand.some(minibrand => (minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') && minibrand.seriesNo.toString().toLowerCase().replace(/\s/g, ''))
+// //       === 
+// //       (minibrandItemNo.toString().toLowerCase().replace(/\s/g, '') && minibrandSeriesNo.toString().toLowerCase().replace(/\s/g, ''))
+// //       );
     
-    if(found) {
-        res.json(minibrand.filter(minibrand => minibrand.rarity.toString().toLowerCase().replace(/\s/g, '') === minibrandRarity.toString().toLowerCase().replace(/\s/g, '')));
-    } else {
-        res.status(400).json(
-            {
-                ErrorMsg: `${_.startCase(_.toLower(minibrandRarity))} is not a rarity in our database.` 
-            }
-        );
-    }
-});
+// //     if(found) {
+// //         res.json(minibrand.filter(minibrand => minibrand.itemNo.toString().toLowerCase().replace(/\s/g, '') === minibrandItemNo.toString().toLowerCase().replace(/\s/g, '')));
+// //     } else {
+// //         res.status(400).json(
+// //             {
+// //                 ErrorMsg: `${_.startCase(_.toLower(minibrandItemNo))} is not a itemNo in our database.` 
+// //             }
+// //         );
+// //     }
+// // });
+
+// //find mini brands by rarity
+// router.get('/rarity/:rarity', (req, res) => {
+//     const minibrandRarity = req.params.rarity;
+//     const found = minibrand.some(minibrand => minibrand.rarity.toString().toLowerCase().replace(/\s/g, '') === minibrandRarity.toString().toLowerCase().replace(/\s/g, ''));
+    
+//     if(found) {
+//         res.json(minibrand.filter(minibrand => minibrand.rarity.toString().toLowerCase().replace(/\s/g, '') === minibrandRarity.toString().toLowerCase().replace(/\s/g, '')));
+//     } else {
+//         res.status(400).json(
+//             {
+//                 ErrorMsg: `${_.startCase(_.toLower(minibrandRarity))} is not a rarity in our database.` 
+//             }
+//         );
+//     }
+// });
 
 
 
